3ea08974f9edc8a18a492e08e78c1713e0e1dcb4
syrinezbidi <syrinezbidi@tudelft.nl>
2026-03-19 15:25:02 +0100

    0.1.0

--- industrial_reconstruction/setup.py
+++ industrial_reconstruction/setup.py
@@ -7,7 +7,7 @@ package_name = 'industrial_reconstruction'
 
 setup(
     name=package_name,
-    version='0.0.1',
+    version='0.1.0',
     packages=['industrial_reconstruction', 'src/industrial_reconstruction/utility'],
     data_files=[
     ('share/ament_index/resource_index/packages',
